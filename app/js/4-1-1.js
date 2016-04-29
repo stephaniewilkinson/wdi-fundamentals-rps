9dbfa10c2dda8c9f22613f72a0b4bf9e5de22a6a
Stephanie Wilkinson <what.happens@gmail.com>
2016-04-29 15:41:55 -0700

    adds fizzbuzz in js

---
+++ app/js/4-1-1.js
@@ -0,0 +1,14 @@
+var x
+
+var result;
+
+if (x % 15 === 0) {
+  result = "fizzbuzz"
+  }
+else if (x % 3 === 0) {
+  result = "fizz";
+  }
+else if (x % 5 === 0) {
+  result = "buzz"
+  }
+else result = x
