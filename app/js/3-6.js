09423feb17541b492ab18179993013dc8b024ce8
Stephanie Wilkinson <what.happens@gmail.com>
2016-04-29 15:24:29 -0700

    writes a function for randomPlay()

--- app/js/3-6.js
+++ app/js/3-6.js
@@ -9,20 +9,13 @@ var getPlayerMove = function(move) {
 }
 
 
-
-
-
-
-
 var getComputerMove = function(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
-}
 
-
-var anotherData;
-if (typeof(anotherData) == 'undefined') {
-  console.log('No definition found');
+    if (!move) {
+      randomPlay();
+    }
+    return move;
 }
